eba82348f90edec5d23fa0f695a731ef24e0bef4
Ethan Roy <ethanroy395@gmail.com>
2020-04-27 08:52:17 -0700

    create user table for session tracking

---
+++ userHandler.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const uuid = require('uuid');
+const AWS = require('aws-sdk');
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const uuidv4 = require('uuid/v4')
+
+
+module.exports.updateSessions = (event, context, callback) => {
+
+  //update data in exisitng entry in the dynamoDB table by sessionID
+  //test comment
+  const data = JSON.parse(event.body);
+  const params = {
+    TableName: process.env.USER_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },   
+    ExpressionAttributeNames: {'#sessions': 'sessions'}, //COLUMN NAME 
+    ExpressionAttributeValues: { ':session': dynamoDb.createSet([data.session])},
+    UpdateExpression: "ADD #sessions :session"
+  }
+  
+  const headers = {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true};
+
+     dynamoDb.update(params).promise()
+      .then(result => {
+        const response = {
+          statusCode: 200,
+          headers: headers,
+          body: JSON.stringify(params.Item),
+        };
+        callback(null, response);
+      })
+      .catch(error => {
+        console.error(error);
+        callback(new Error('Couldn\'t update sessions.'));
+        return;
+      });
+};
